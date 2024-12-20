fd8ad1c3e561e30b5986bd06bc582403d0366591
manish-involead <manish.soni@involead.com>
2024-12-20 08:04:49 +0530

    Adding home page

---
+++ client/src/types/fieldEnum.ts
@@ -0,0 +1,14 @@
+export enum PriorityType {
+  Urgent = "Urgent",
+  High = "High",
+  Medium = "Medium",
+  Low = "Low",
+  Backlog = "Backlog",
+}
+
+export enum StatusType {
+  ToDo = "To Do",
+  WorkInProgress = "Work In Progress",
+  UnderReview = "Under Review",
+  Completed = "Completed",
+}
